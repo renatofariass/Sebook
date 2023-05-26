084dedd6bc0630921ef47657ac8bdec9798a9087
Renato Farias <santosrenato351@gmail.com>
2023-05-25 21:11:50 -0300

    habilitando cors

--- src/main/java/br/com/sebook/resources/CategoriaResource.java
+++ src/main/java/br/com/sebook/resources/CategoriaResource.java
@@ -8,10 +8,7 @@ import br.com.sebook.mapper.Mapper;
 import br.com.sebook.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -21,6 +18,7 @@ public class CategoriaResource {
     @Autowired
     CategoriaService service;
 
+    @CrossOrigin(origins = "*")
     @GetMapping
     public ResponseEntity<List<CategoriaMinDto>> findAll() {
         List<Categoria> lista = service.findAll();
