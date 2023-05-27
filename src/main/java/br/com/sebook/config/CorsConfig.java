ee2033772336a54eacc7befd4757a76cc97b7a17
Renato Farias <santosrenato351@gmail.com>
2023-05-27 15:42:50 -0300

    liberando metodos put e post

--- src/main/java/br/com/sebook/config/CorsConfig.java
+++ src/main/java/br/com/sebook/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedHeaders("*")
-                .allowedMethods("GET");
+                .allowedMethods("GET, POST", "PUT");
     }
 }
 
