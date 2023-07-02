04757fc8fcc2498a7cadbc89826a73c724f118d3
Renato Farias <santosrenato351@gmail.com>
2023-07-01 21:30:04 -0300

    adicionando hash as senhas

---
+++ src/main/resources/db/migration/V10__Populate_Table_Dono.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_usuarios (login, senha)
+VALUES ('renato@gmail.com', '$2a$12$o8Yu02C3xTNkAMfbsKKgS.oRlpQXWuVGFuigwBUqjmlmzaEqraoei'),
+       ('madu@gmail.com', '$2a$12$SOpSUv4w.S2Igq0N9A5Yk.Cdre3UrvRdqbkOYQMgOBrDAm73F69u6'),
+       ('joao@gmail.com', '$2a$12$aXpCucCr1cYhWbQ64Kx7eObprXtWmzj/igZ/raIxNXRh1pXJlLDiq');
--- src/main/resources/db/migration/V7__Populate_Table_Dono.sql
+++ src/main/resources/db/migration/V7__Populate_Table_Dono.sql
@@ -1,4 +1,7 @@
 INSERT INTO tb_donos (nome, cpf, email, senha)
-VALUES ('Renato Alberto', '123.456.789-01', 'renato1@gmail.com', '1234'),
-       ('Madu Gomes', '123.456.789-02', 'madu1@gmail.com', '1234'),
-       ('João Valença', '123.456.789-03', 'joao1@gmail.com', '1234');
+VALUES ('Renato Alberto', '123.456.789-01', 'renato1@gmail.com',
+        '$2a$12$o8Yu02C3xTNkAMfbsKKgS.oRlpQXWuVGFuigwBUqjmlmzaEqraoei'),
+       ('Madu Gomes', '123.456.789-02', 'madu1@gmail.com',
+        '$2a$12$SOpSUv4w.S2Igq0N9A5Yk.Cdre3UrvRdqbkOYQMgOBrDAm73F69u6'),
+       ('João Valença', '123.456.789-03', 'joao1@gmail.com',
+        '$2a$12$aXpCucCr1cYhWbQ64Kx7eObprXtWmzj/igZ/raIxNXRh1pXJlLDiq');
